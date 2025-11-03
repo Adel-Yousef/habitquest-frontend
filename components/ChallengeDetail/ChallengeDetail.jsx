04ff1a202cd632f20255b54f0690363c8bdeae67
Adel-Yousef <adelyusif9@gmail.com>
2025-11-03 12:56:01 +0300

    Did the join challenge and now the user can join a challenge

--- components/ChallengeDetail/ChallengeDetail.jsx
+++ components/ChallengeDetail/ChallengeDetail.jsx
@@ -4,11 +4,24 @@ import { Link, useParams } from "react-router"
 
 
 
+
 function ChallengeDetail() {
     
     const {challengeId} = useParams()
     const [challenge, setChallenge] = useState({})
     const [errors, setErrors] = useState(null)
+    const [joinMessage, setJoinMessage] = useState('')
+
+    async function joinChallenge() {
+    try {
+        const response = await axios.post(`http://127.0.0.1:8000/api/challenges/${challengeId}/participations/`, {})
+        setJoinMessage("Successfully joined challenge!")
+        getSingleChallenge()
+    } catch (error) {
+        console.log('full error:', error.response)
+        setJoinMessage(error.response?.data?.error || "Error joining challenge")
+    }
+}
 
     async function getSingleChallenge() {
         try{
@@ -37,6 +50,8 @@ function ChallengeDetail() {
         <p>Duration: {challenge.start_date} to {challenge.end_date}</p>
         <p>Created by: User {challenge.created_by}</p>
 
+        <button onClick={joinChallenge}>Join Challenge!</button>
+        {joinMessage && <p>{joinMessage}</p> }
         <div>
             <h3>Participations:</h3>
             {
